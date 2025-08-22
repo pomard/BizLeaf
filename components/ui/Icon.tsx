86c0de2a6429437158092fc204532138080ceb9f
pomard <hukinoyou.0425@gmail.com>
2025-08-22 13:58:07 +0900

    vercel error 修正

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -2,9 +2,10 @@ interface IconProps {
   name: string;
   size?: number;
   className?: string;
+  style?: React.CSSProperties;
 }
 
-export default function Icon({ name, size = 20, className = '' }: IconProps) {
+export default function Icon({ name, size = 20, className = '', style }: IconProps) {
   return (
     <img 
       src={`/icons/${name}.svg`}
@@ -12,7 +13,7 @@ export default function Icon({ name, size = 20, className = '' }: IconProps) {
       width={size}
       height={size}
       className={className}
-      style={{ display: 'inline-block', verticalAlign: 'middle' }}
+      style={{ display: 'inline-block', verticalAlign: 'middle', ...style }}
     />
   );
 }
